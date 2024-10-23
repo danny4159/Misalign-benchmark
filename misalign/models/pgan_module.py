d6f7d1f4d42fc96585cd1255a7f6e6f7ad9a63e9
Kim Daniel <danny4159@naver.com>
2024-10-24 08:57:02 +0900

    pgan_ctxLoss

--- misalign/models/pgan_module.py
+++ misalign/models/pgan_module.py
@@ -9,6 +9,10 @@ from torchvision import models
 
 from misalign.data.components.transforms_fly import dataset_IXI_FLY, dataset_synthRAD_FLY_RAM, dataset_synthRAD_FLY
 from torch.utils.data import DataLoader
+from misalign.models.components.contextual_loss import (
+    Contextual_Loss,
+)  # this is the CX loss
+
 
 import higher
 
@@ -53,6 +57,10 @@ class PixelGANModule(BaseModule):
         self.criterionGAN = GANLoss(use_lsgan=not self.no_lsgan)
         self.criterionL1 = torch.nn.L1Loss(reduction="none")
 
+        style_feat_layers = {"conv_2_2": 1.0, "conv_3_2": 1.0, "conv_4_2": 1.0}
+        if self.params.lambda_style != 0:
+            self.criterionStyle = Contextual_Loss(style_feat_layers, cobi=True)
+
         # Image Pool
         self.fake_A_pool = ImagePool(params.pool_size)
         self.fake_B_pool = ImagePool(params.pool_size)
@@ -83,7 +91,7 @@ class PixelGANModule(BaseModule):
           f"meta_real_b: Min = {meta_real_b.min().item()}, Max = {meta_real_b.max().item()}, "
           f"meta_msk: Min = {meta_msk.min().item()}, Max = {meta_msk.max().item()}")
     
-    def backward_G(self, real_a, real_b, fake_a, fake_b, lambda_l1, lambda_vgg, weight_a=None, weight_b=None):
+    def backward_G(self, real_a, real_b, fake_a, fake_b, lambda_l1, lambda_vgg, lambda_style, weight_a=None, weight_b=None):
         loss_G = torch.zeros(1, device=self.device, requires_grad=True)
 
         if weight_a is not None:
@@ -169,6 +177,25 @@ class PixelGANModule(BaseModule):
         self.log("loss_VGG", VGG_loss.detach(), prog_bar=True)  # VGG loss 로그 추가
         loss_G = loss_G + VGG_loss
 
+        if self.params.lambda_style != 0:
+            Style_loss_A = self.criterionStyle(real_a, fake_a) * lambda_style
+            if weight_b is not None:
+                Style_loss_A = torch.mean(Style_loss_A * weight_batch_b)
+            else:
+                Style_loss_A = torch.mean(Style_loss_A)
+            self.check_nan(Style_loss_A, "Style_loss_A")
+
+            Style_loss_B = self.criterionStyle(real_b, fake_b) * lambda_style
+            if weight_a is not None:
+                Style_loss_B = torch.mean(Style_loss_B * weight_batch_a)
+            else:
+                Style_loss_B = torch.mean(Style_loss_B)
+            self.check_nan(Style_loss_B, "Style_loss_B")
+            
+            Style_loss = (Style_loss_A + Style_loss_B) * 0.5
+            self.log("loss_Style", Style_loss.detach(), prog_bar=True)
+            loss_G = loss_G + Style_loss
+            
         return loss_G
     
     def check_model_weights(self, model, model_name="Model"):
@@ -404,7 +431,7 @@ class PixelGANModule(BaseModule):
 
         with optimizer_G.toggle_model():
             real_a, real_b, fake_a, fake_b = self.model_step(batch)
-            loss_G = self.backward_G(real_a, real_b, fake_a, fake_b, self.params.lambda_l1, self.params.lambda_vgg, weight_a=w_a, weight_b=w_b)
+            loss_G = self.backward_G(real_a, real_b, fake_a, fake_b, self.params.lambda_l1, self.params.lambda_vgg, self.params.lambda_style, weight_a=w_a, weight_b=w_b)
             self.manual_backward(loss_G)
             self.clip_gradients(optimizer_G, gradient_clip_val=0.5, gradient_clip_algorithm="norm")
             optimizer_G.step()
